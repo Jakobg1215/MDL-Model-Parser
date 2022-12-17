dadc69c73aacc7362ace245bf5455e7a2582abc0
jakobg1215 <jakobg1215@gmail.com>
2022-12-16 23:13:33 -0600

    Add PHY File Parcer

---
+++ src/PhyReaders/ReadSwapCompactSurfaceHeader.ts
@@ -0,0 +1,33 @@
+import FileReader from '../FileReader';
+import type Vector3 from '../Vector3';
+import ReadLegacySurfaceHeader from './ReadLegacySurfaceHeader';
+
+class ReadSwapCompactSurfaceHeader {
+    public readonly size: number;
+    public readonly vphysicsID: number;
+    public readonly version: number;
+    public readonly modelType: number;
+    public readonly surfaceSize: number;
+    public readonly dragAxisAreas: Vector3;
+    public readonly axisMapSize: number;
+
+    public readonly surface: ReadLegacySurfaceHeader | null = null;
+
+    public constructor(file: FileReader) {
+        this.size = file.readInt();
+        this.vphysicsID = file.readInt();
+        this.version = file.readShort();
+        this.modelType = file.readShort();
+        this.surfaceSize = file.readInt();
+        this.dragAxisAreas = file.readVector3();
+        this.axisMapSize = file.readInt();
+
+        if (this.vphysicsID !== 0x59485056) {
+            this.surface = new ReadLegacySurfaceHeader(file);
+        }
+
+        file.setOffset(file.fileReadOffset + this.surfaceSize);
+    }
+}
+
+export default ReadSwapCompactSurfaceHeader;
