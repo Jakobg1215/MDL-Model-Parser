461ce5a58bd2ab20cbe555b825f454b8ea8fa331
Jakobg1215 <jakobg1215@gmail.com>
2023-01-04 08:48:20 -0500

    Add MDL Readers TODO

--- src/ReadMDL.ts
+++ src/ReadMDL.ts
@@ -32,70 +32,146 @@ class ReadMDL {
         console.log('MDL File size: %d', fileSize);
         this.header = new ReadHeader(file);
 
-        // Tried to keep the order of the readers the same as the order of the MDL file.
+        // Tried to keep the order of the readers the same as the order of StudioMDL.
+
+        /* Header 2 */
 
         // From looking at the source code, header2 may be optional.
         this.header2 = new ReadHeader2(file);
+
+        /* Bones */
 
         for (let boneReader = 0; boneReader < this.header.numbones; boneReader++) {
             file.setOffset(this.header.boneindex + boneReader * 216);
             this.bones.push(new ReadBone(file)); // Done
         }
 
+        /* Bone Controllers */
+
         for (let boneControllerReader = 0; boneControllerReader < this.header.numbonecontrollers; boneControllerReader++) {
             file.setOffset(this.header.bonecontrollerindex + boneControllerReader * 56);
             this.boneControllers.push(new ReadBoneController(file)); // Done
         }
+
+        /* Attachments */
 
         for (let localAttachmentReader = 0; localAttachmentReader < this.header.numlocalattachments; localAttachmentReader++) {
             file.setOffset(this.header.localattachmentindex + localAttachmentReader * 92);
             this.localAttachments.push(new ReadAttachment(file)); // Done
         }
 
+        /* Hitbox Sets */
+
         for (let hitboxSetReader = 0; hitboxSetReader < this.header.numhitboxsets; hitboxSetReader++) {
             file.setOffset(this.header.hitboxsetindex + hitboxSetReader * 12);
             this.hitboxSets.push(new ReadHitboxSet(file)); // Done
         }
 
+        /* Bone Table */
+
         // What is this for?
         file.setOffset(this.header.bonetablebynameindex);
         this.boneTableByName = file.readByteArray(this.header.numbones); // This is not reading all the bytes
+
+        /* Animations */
 
         for (let localAnimationReader = 0; localAnimationReader < this.header.numlocalanim; localAnimationReader++) {
             file.setOffset(this.header.localanimindex + localAnimationReader * 100);
             this.localAnimations.push(new ReadAnimationDescription(file)); // Not Done
         }
 
-        console.log('MDL Read Bytes: %d', file.readByteCount + 3);
+        /* Sequences */
 
         // TODO: Add sequence reader
 
+        /* Node Name */
+
+        /* Nodes */
+
         // TODO: Add nodes reader
+
+        /* Body Parts */
 
         for (let bodyPartReader = 0; bodyPartReader < this.header.numbodyparts; bodyPartReader++) {
             file.setOffset(this.header.bodypartindex + bodyPartReader * 16);
             this.bodyParts.push(new ReadBodyParts(file)); // Not Done
         }
 
+        /* Flex Discriptions */
+
         file.setOffset(this.header.flexdescindex);
         for (let flexDescriptionReader = 0; flexDescriptionReader < this.header.numflexdesc; flexDescriptionReader++) {
             this.flexDescriptions.push(new ReadFlexDescription(file)); // Done
         }
+
+        /* FLex Controllers */
 
         file.setOffset(this.header.flexcontrollerindex);
         for (let flexControllerReader = 0; flexControllerReader < this.header.numflexcontrollers; flexControllerReader++) {
             this.flexControllers.push(new ReadFlexController(file)); // Done
         }
 
-        file.setOffset(this.header.flexcontrolleruiindex);
-        for (let flexControllerUIReader = 0; flexControllerUIReader < this.header.numflexcontrollerui; flexControllerUIReader++) {
-            this.flexControllerUIs.push(new ReadFlexControllerUI(file)); // Done
-        }
+        /* Flex Rules */
 
         for (let flexRuleReader = 0; flexRuleReader < this.header.numflexrules; flexRuleReader++) {
             file.setOffset(this.header.flexruleindex + flexRuleReader * 12);
             this.flexRules.push(new ReadFlexRule(file)); // Not Done
         }
+
+        /* Flex Controller UI */
+
+        file.setOffset(this.header.flexcontrolleruiindex);
+        for (let flexControllerUIReader = 0; flexControllerUIReader < this.header.numflexcontrollerui; flexControllerUIReader++) {
+            this.flexControllerUIs.push(new ReadFlexControllerUI(file)); // Done
+        }
+
+        /* IK Chains */
+
+        // TODO: Add IK Chains reader
+
+        /* IK Locks */
+
+        // TODO: Add IK Locks reader
+
+        /* Mouths */
+
+        // TODO: Add Mouths reader
+
+        /* Pose Parameters */
+
+        // TODO: Add Pose Parameters reader
+
+        /* Model Groups */
+
+        // TODO: Add Model Groups reader
+
+        /* Animation Blocks */
+
+        // TODO: Add Animation Blocks reader
+
+        /* Textures */
+
+        // TODO: Add Textures reader
+
+        /* Texture Directories */
+
+        // TODO: Add Texture Directories reader
+
+        /* Texture Families */
+
+        // TODO: Add Texture Families reader
+
+        /* Key Values */
+
+        // TODO: Add Key Values reader
+
+        /* Bone Transformations */
+
+        // TODO: Add Bone Transformations reader
+
+        /* Bone Flex Drivers */
+
+        // TODO: Add Bone Flex Drivers reader
 
         console.log('MDL Read Bytes: %d, %d unread bytes', file.readByteCount, fileSize - file.readByteCount);
     }
