92e1b947f9e93c3f1c6453593ac84b989e74e46a
Jakobg1215 <jakobg1215@gmail.com>
2023-01-04 11:02:34 -0500

    Change Test Model

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,19 @@ import ReadMDL from './ReadMDL';
 import ReadVTX from './ReadVTX';
 import ReadVVD from './ReadVVD';
 
-// This is using the model mossman from Garry's Mod as a base
+// This is using the model heavy from Team Fortress 2 as a base
 // This can be changed to any model you want for testing
 
-const dx80VTXData = new ReadVTX(new FileReader(readFileSync('models/mossman.dx80.vtx')));
-const dx90VTXData = new ReadVTX(new FileReader(readFileSync('models/mossman.dx90.vtx')));
-const mdlData = new ReadMDL(new FileReader(readFileSync('models/mossman.mdl')));
-const phyData = new ReadPhy(new FileReader(readFileSync('models/mossman.phy')));
-const VVDData = new ReadVVD(new FileReader(readFileSync('models/mossman.vvd')));
+const dx80VTXData = new ReadVTX(new FileReader(readFileSync('models/heavy.dx80.vtx')));
+const dx90VTXData = new ReadVTX(new FileReader(readFileSync('models/heavy.dx90.vtx')));
+const swVTXData = new ReadVTX(new FileReader(readFileSync('models/heavy.sw.vtx')));
+const mdlData = new ReadMDL(new FileReader(readFileSync('models/heavy.mdl')));
+const phyData = new ReadPhy(new FileReader(readFileSync('models/heavy.phy')));
+const VVDData = new ReadVVD(new FileReader(readFileSync('models/heavy.vvd')));
 
-writeFileSync('models/json/mossman.dx80.vtx.json', dx80VTXData.toJSON());
-writeFileSync('models/json/mossman.dx90.vtx.json', dx90VTXData.toJSON());
-writeFileSync('models/json/mossman.mdl.json', mdlData.toJSON());
-writeFileSync('models/json/mossman.phy.json', phyData.toJSON());
-writeFileSync('models/json/mossman.vvd.json', VVDData.toJSON());
+writeFileSync('models/json/heavy.dx80.vtx.json', dx80VTXData.toJSON());
+writeFileSync('models/json/heavy.dx90.vtx.json', dx90VTXData.toJSON());
+writeFileSync('models/json/heavy.sw.vtx.json', swVTXData.toJSON());
+writeFileSync('models/json/heavy.mdl.json', mdlData.toJSON());
+writeFileSync('models/json/heavy.phy.json', phyData.toJSON());
+writeFileSync('models/json/heavy.vvd.json', VVDData.toJSON());
